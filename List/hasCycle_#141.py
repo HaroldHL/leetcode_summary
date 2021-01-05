afa5b6db6925e109e68e40077406b6cb4a4cbfe1
HaroldYin <haroldyin@outlook.com>
2021-01-05 23:24:13 +0800

    update list

---
+++ List/hasCycle_#141.py
@@ -0,0 +1,15 @@
+def hasCycle(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return False
+        
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+            # tip:
+            # is 判断两个变量是否是引用同一个内存地址。
+            # == 判断两个变量是否相等
+            if slow is fast:
+                return True
+
+        return False   
