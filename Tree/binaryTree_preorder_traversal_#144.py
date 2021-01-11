4fcbcf8e4aa4779e2d109144b25a5044f46ce093
HaroldYin <haroldyin@outlook.com>
2021-01-11 12:06:10 +0800

    Update tree

--- Tree/binaryTree_preorder_traversal_#144.py
+++ Tree/binaryTree_preorder_traversal_#144.py
@@ -4,6 +4,10 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# 递归
+# 时间复杂度：O(n)，n为节点数，访问每个节点恰好一次。
+# 空间复杂度：空间复杂度：O(h)，h为树的高度。最坏情况下需要空间O(n)，平均情况为O(logn)
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
         res = []
